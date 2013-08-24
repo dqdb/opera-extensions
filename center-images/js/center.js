35f5b9fdfc85c52a9df8c3c78fee38e5437fd26c
dqdb <dqdb@freemail.hu>
2013-08-24 21:55:33 +0200

    Center Images 1.0.0

---
+++ center-images/js/center.js
@@ -0,0 +1,18 @@
+﻿var image = document.body.firstElementChild;
+if (image && image.tagName == "IMG" && image.src == window.location.href)
+{
+	// comment this line or change background color to the desired value
+	document.body.style.background = "#404040";
+	
+	var centerImage = function(e)
+	{
+		var marginLeft = Math.max((window.innerWidth - image.width) >> 1, 0);
+		var marginTop = Math.max((window.innerHeight - image.height) >> 1, 0);
+		image.style.marginTop = marginTop + "px";
+		image.style.marginLeft = marginLeft + "px";
+	};
+	
+	image.addEventListener("click", centerImage);
+	window.addEventListener("resize", centerImage);
+	centerImage();
+}
---
+++ center-images/readme.txt
@@ -0,0 +1,7 @@
+Center Images
+Copyright (c) 2013 dqdb
+
+This extension centers viewed images in Opera 15 and changes page background color.
+
+Extension icon copyright by dAKirby309
+http://www.iconarchive.com/show/windows-8-metro-icons-by-dakirby309/Apps-Picture-Viewer-Metro-icon.html
