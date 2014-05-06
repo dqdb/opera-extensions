f4a39e4033c450d8d1049a5c92a2e5307e4c6220
dqdb <dqdb@freemail.hu>
2014-05-06 18:27:22 +0200

    packed extension

--- google-search-fix/background.js
+++ google-search-fix/background.js
@@ -13,17 +13,17 @@ chrome.webRequest.onBeforeSendHeaders.addListener(function (details)
 	{
 		var host = matches[2];
  		if (host.indexOf("google") != -1 || host.indexOf("goo.gl") != -1)
-        {
-        	for (var n = 0; n < headers.length; n++)
-        	{
-        		var p = headers[n];
-        		if (p.name == "User-Agent")
-        		{
-        			p.value = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36";
-        			break;
-        		}
-        	}
-        }
+		{
+			for (var n = 0; n < headers.length; n++)
+			{
+				var p = headers[n];
+				if (p.name == "User-Agent")
+				{
+					p.value = "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36";
+					break;
+				}
+			}
+		}
 	}
 	
 	return { requestHeaders: headers };
